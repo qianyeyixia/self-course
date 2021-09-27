cbeaa93b62d09116bf9eac594e108f6a37739ca5
qianyeyixia <1521341740@qq.com>
2021-09-27 22:33:20 +0800

    实现简单的vue router

--- src/SRouter/index.js
+++ src/SRouter/index.js
@@ -2,9 +2,32 @@
 // import Vue from "vue";
 let Vue
 class VueRouter {
-    constructor() {
+    constructor(options) {
         // Vue
+        //1.保存路由选项
+        this.$options = options;
+        //初始化 current
+        const inital = window.location.hash.slice(1) || "/"
+        // Vue.util.defineReactive 可以给当前的对象指定一个响应式的属性
+        // 参考 vue.runtime.esm.js  Vue.util  public方法
+        Vue.util.defineReactive(this, 'current', inital)
+    // 2.监控 href变化
+        window.addEventListener('hashchange', () => {
+            //  例子  hash:#/about
+            this.current = window.location.hash.slice(1)
+            console.log(this.current)
+            console.log(Vue.util)
+        })
+        // 方法2 初始化 current
+        // window.addEventListener('load', () => {
+        //     this.current = "/"
+        // })
+
+
     }
+
+
+
 }
 // 参数1 是 vue 的构造函数
 
@@ -14,7 +37,7 @@ VueRouter.install = function (_Vue) {
     // 1。 注册 $router. 让所有的组件实例都可以访问
     // 混入： Vue.mixin({})
     Vue.mixin({
-        beforreCreate() {
+        beforeCreate() {
             // 延迟执行, 延迟到vue和 router实例创建完毕
             if(this.$options.router) {
                 Vue.prototype.$router = this.$options.router
@@ -34,17 +57,34 @@ VueRouter.install = function (_Vue) {
             }
         },
         render(h) {
-            console.log(this)
+            // <a href=""#/home>xxxx</a>
+            // h 是render函数调用时，框架传入的 createElement
+            // 等同于 React中的 createElement, 返回 vdom\
             return h('a', {
                 attrs: {
                     href: `#${this.to}`
                 }
             },this.$slots.default)
+            /*
+            * 等同与 <a href={'#'+ this.to}>this.$slots.default</a>
+            *
+            *
+            * */
         }
     })
     Vue.component('router-view', {
         render(h) {
-            return h('div', 'router-view')
+            let component = null;
+            //1. 获取当前的 url的 hash 部分
+            // return h('div', {}, this.$slots.defulat)
+            //2. 根据hash部分从路由表中获取对应的组件
+            const route = this.$router.$options.routes.find((route) =>
+                route.path === this.$router.current
+            );
+            if(route) {
+                component = route.component
+            }
+            return h(component)
         }
     })
 }
